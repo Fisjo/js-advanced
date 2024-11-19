315047c0e87099fbd242276b8cd009341b160e78
Fisjo <ignacio.po.ipo@gmail.com>
2024-11-19 23:52:32 +0100

    Promise hell

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -12,14 +12,29 @@ export const promiseComponent = (element) => {
     const rederHeroe = (hero) => {
         element.innerHTML = hero.name; 
     }
+    const renderTwoHeroes = (hero1, hero2) => {
+        element.innerHTML = `
+            <h4>${hero1.name}</h4>
+            <h4>${hero2.name}</h4>
+        `;
+    }
     const renderError = (error) => {
         element.innerHTML = error; 
     }
 
-    const id1 = '5d86371f9f80b591f499df32'; 
+    const id1 = '5d86371f9f80b591f499df32';
+    const id2 = '5d86371f1efebc31def272e2'; 
+
 
     findHero(id1)
-        .then(rederHeroe)
+        .then((hero1) => {
+            
+            findHero(id2)
+                .then((hero2) => {
+                    renderTwoHeroes(hero1, hero2)
+                })
+                .catch(renderError); 
+        })
         .catch(renderError); 
 
 }
