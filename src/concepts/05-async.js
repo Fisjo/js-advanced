5adbe2de9550bf14e3cf993e9d78559714843911
Fisjo <ignacio.po.ipo@gmail.com>
2024-11-20 22:48:44 +0100

    05-async.js modified

--- src/concepts/05-async.js
+++ src/concepts/05-async.js
@@ -7,11 +7,26 @@ import { heroes } from "../data/heros"
 export const asyncComponent = (element) => {
 
     const id1 = '5d86371f2343e37870b91ef1';
-    findHero(id1).then((name) => element.innerHTML = name);
+    console.log('Inicio componente');
 
+    findHero(id1) //el console.log del then se escribirá despues porque primero ejecuta el codigo sincron
+        .then(name => element.innerHTML = name)
+        .catch((error) => element.innerHTML = error)
+
+    console.log('Fin del componente'); //aqui termina la funcion y se salta a donde manda la promesa
 }
+
 //async cambia una funcion (deja de devolver un objeto y devuelve una promesa)
+/**
+ * 
+ * @param {String} id 
+ * @returns {Promise<String>}
+ */
 const findHero = async (id) => {
     const hero = heroes.find (hero => hero.id === id)
+    if (!hero) {
+        throw new Error(`Hero with id ${id} not found`);
+    }
+    
     return hero.name; 
 }
