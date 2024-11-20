a86832742fee9103c3a67aaef2e10334e4978045
Fisjo <ignacio.po.ipo@gmail.com>
2024-11-20 12:31:53 +0100

    04-promise-race.js added

---
+++ src/concepts/04-promise-race.js
@@ -0,0 +1,37 @@
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const promiseRaceComponent = (element) => {
+
+    element.innerHTML = 'Loading...'
+
+    const renderValue = (value) => {
+        element.innerHTML = value; 
+    }
+    Promise.race([
+        slowPromise(), 
+        midPromise(), 
+        fastPromise(),
+    ]).then(renderValue);
+}
+
+const slowPromise = () => new Promise(resolve => {
+    setTimeout(() => {
+        resolve('Slow Promise');
+    }, 2000);
+})
+
+const midPromise = () => new Promise(resolve => {
+    setTimeout(() => {
+        resolve('Medium Promise');
+    }, 1500);
+})
+
+const fastPromise = () => new Promise(resolve => {
+    setTimeout(() => {
+        resolve('Fast Promise');
+    }, 1000);
+})
