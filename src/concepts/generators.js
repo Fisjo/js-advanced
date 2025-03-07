4da871ec57897f3453b3fa1965f987c942786ca5
Fisjo <ignacio.po.ipo@gmail.com>
2025-03-07 20:14:01 +0100

    ej Funciones Generadoras

--- src/concepts/generators.js
+++ src/concepts/generators.js
@@ -5,16 +5,33 @@
  */
 export const generatorFunctionsComponent = (element) => {
 
-    const myGenerator = myFirstGeneratorFunction(); 
+    // const myGenerator = myFirstGeneratorFunction(); 
+    // console.log(myGenerator.next());
 
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
+    const genId = idGenerator(); 
+
+    const button = document.createElement('button'); 
+    button.innerText = 'Click me'; 
+    element.append(button); 
+
+    const renderButton = () => {
+        const {value} = genId.next(); 
+        button.innerText = `Click ${value}`; 
+    }
+
+    button.addEventListener('click', (event) => renderButton());
+    console.log(genId.next());
+    
     
 }
+
+function* idGenerator() {
+    let currentId = 0; 
+    while (true) {
+        yield ++ currentId; 
+    }
+}
+
 
 function* myFirstGeneratorFunction () {
     yield 'Mi primer valor'; 
